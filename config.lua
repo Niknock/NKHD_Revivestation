a3b0e9ef5c23880405d4a84afa246ee923b98a55
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-12-18 21:00:03 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,18 +1,22 @@
 Config = {}
 
-Config.ESX = 'new' -- new or old
+Config.ESX = 'new'                                                  -- new or old
 
-Config.Locale = 'en' -- en, de or custom
+Config.Locale = 'en'                                                -- en, de or custom
 
-Config.Debug = false -- Debug is only German!
+Config.Debug = false
 
-Config.ReviveLocation = { x = 0.0, y = 0.0, z = 0.0 }
-Config.HealLocation = { x = 0.0, y = 0.0, z = 0.0 }
+Config.ReviveLocation = { x = 0.0, y = 0.0, z = 0.0 } 
+Config.HealLocation = { x = 0.0, y = 0.0, z = 0.0 } 
 
-Config.ReviveCost = 1000 -- Kosten für das Reviven
-Config.HealCost = 500 -- Kosten für das Healen
+Config.ReviveCost = 1000                                            -- How much you should Pay if you get Revived
+Config.HealCost = 500                                               -- How much you should Pay if you get Healed
+Config.UseOKOKBilling = true                                        -- true if you use OKOK Billing
+Config.ReviveCostOKOK = '1000'                                      -- How much you should Pay if you get Revived | Change only if you use OKOK Billing
+Config.HealCostOKOK = '500'                                         -- How much you should Pay if you get Healed | Change only if you use OKOK Billing
 
-Config.JobAccess = "ambulance" -- Beruf, der Zugriff auf den Befehl hat
+Config.UseCommand = false                                           -- true if an Medic should use an Command to activate the Triggers | if false script will search if an Person with the Job in Config.JobAccess is online if not the triggers automaticly get spawned
+Config.JobAccess = "ambulance"                                      -- Jobs, which can use the Command or get automaticly searched
 
 Config.Marker = {
     Type = 1,
